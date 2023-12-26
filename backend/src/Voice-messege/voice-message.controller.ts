0aaa27d7d1f2044d522e378508d3ad8cd0ae5d40
APPMAN M-078 <admin@APPMAN-M-078.local>
2023-12-26 22:48:12 +0700

    download video, convert file from mp3 to mp4, send to s3

--- backend/src/Voice-messege/voice-message.controller.ts
+++ backend/src/Voice-messege/voice-message.controller.ts
@@ -1,36 +1,109 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import express, { Request, Response } from "express";
-import youtubedl from "youtube-dl-exec";
+import dl from "youtube-dl-exec";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import path from "path";
+import multer from "multer"; 
+import fs from "fs/promises";
+import ytdl from "ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = express.Router();
-
+const currentDate = new Date().toISOString().split("T")[0]; 
 let link: string;
+const s3Key: string = `videos/${currentDate}`;
+
+const bucketName = "voice-messenger";
+const region = "ap-southeast-1";
+const accessKeyId = "AKIA5DP55HDAB4JV73M5";
+const secretAccessKey = "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";
+
+const s3Client = new S3Client({
+	region,
+	credentials: {
+		accessKeyId,
+		secretAccessKey,
+	},
+});
 
 router.post("/youtube-link", async (req: Request, res: Response) => {
 	try {
-		({ link } = req.body);
-		res.json(link);
-		console.log(link);
-
-		const outputTemplate = path.join(__dirname, "downloads", "%(fulltitle)s.%(ext)s");
-
-		await youtubedl(link, {
-			dumpSingleJson: true,
-			noCheckCertificates: true,
-			noWarnings: true,
-			preferFreeFormats: true,
-			addHeader: ["referer:youtube.com", "user-agent:googlebot"]
-		}).then(output => console.log(output.fulltitle));
-
-		await youtubedl(link, {
-			"output": outputTemplate
-		});
-
-		console.log("Audio downloaded successfully! 🔊🎉");
-
+	   ({ link } = req.body);
+	   res.json(link);
+	   console.log(link);
+ 
+	   // Download audio from YouTube
+	   const videoFilePath = await downloadAudio(link);
+ 
+	   // Upload the MP3 file to S3
+	   const key = `videos/${currentDate}/${path.basename(videoFilePath)}`;
+	   await uploadToS3(videoFilePath, key);
+ 
+	   console.log("Downloaded, converted, and uploaded to S3 successfully! 🎉");
 	} catch (error) {
-		console.log(error);
+	   console.error(error);
+	   res.status(500).json({ error: "Internal Server Error" });
 	}
 });
+ 
+const downloadAudio = async (youtubeUrl: string): Promise<string> => {
+	try {
+	   const videoInfo = await ytdl.getInfo(youtubeUrl);
+	   const audioFormat = ytdl.filterFormats(videoInfo.formats, "audioonly")[0];
+ 
+	   if (!audioFormat) {
+		  throw new Error("No audio stream found in the video.");
+	   }
+ 
+	   const audioStream = ytdl(youtubeUrl, { format: audioFormat });
+	   const outputFilePath = `${__dirname}/downloads/${videoInfo.videoDetails.title}.mp3`;
+ 
+	   const proc = ffmpeg({ source: audioStream })
+		  .withAudioCodec("libmp3lame")
+		  .toFormat("mp3")
+		  .on("end", () => {
+			 console.log("Video successfully converted to MP3");
+		  })
+		  .on("error", (error, stdout, stderr) => {
+			 console.error("Error converting video to MP3:", error);
+			 console.error("ffmpeg stdout:", stdout);
+			 console.error("ffmpeg stderr:", stderr);
+		  });
+ 
+	   await new Promise<void>((resolve, reject) => {
+		  proc.saveToFile(outputFilePath)
+			 .on("end", resolve)
+			 .on("error", reject);
+	   });
+ 
+	   return outputFilePath;
+ 
+	} catch (error) {
+	   console.error("Error downloading audio:", error);
+	   throw error;
+	}
+};
+ 
+ 
+const uploadToS3 = async (videoFilePath: string, key: string) => {
+	const uploadParams = {
+	   Bucket: bucketName,
+	   Key: key,
+	   Body: await fs.readFile(videoFilePath), // Read the MP3 file from the local path
+	};
+ 
+	try {
+	   const uploadResult = await s3Client.send(new PutObjectCommand(uploadParams));
+	   console.log("MP3 file uploaded to S3:", uploadResult);
+	} catch (error) {
+	   console.error("Error uploading MP3 file to S3:", error);
+	   throw error;
+	}
+};
+ 
+
 
 export default router;
