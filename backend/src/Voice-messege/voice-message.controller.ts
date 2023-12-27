738de7dedfe16753ad4d232a6c14f17034ccd2be
APPMAN M-078 <admin@APPMAN-M-078.local>
2023-12-27 14:09:31 +0700

    delete when uploaded to s3 successfully

--- backend/src/Voice-messege/voice-message.controller.ts
+++ backend/src/Voice-messege/voice-message.controller.ts
@@ -1,20 +1,15 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import express, { Request, Response } from "express";
-import dl from "youtube-dl-exec";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectAclCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl} from "@aws-sdk/s3-request-presigner";
 import path from "path";
-import multer from "multer"; 
 import fs from "fs/promises";
 import ytdl from "ytdl-core";
 import ffmpeg from "fluent-ffmpeg";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
 const router = express.Router();
 const currentDate = new Date().toISOString().split("T")[0]; 
 let link: string;
-const s3Key: string = `videos/${currentDate}`;
 
 const bucketName = "voice-messenger";
 const region = "ap-southeast-1";
@@ -36,11 +31,11 @@ router.post("/youtube-link", async (req: Request, res: Response) => {
 	   console.log(link);
  
 	   // Download audio from YouTube
-	   const videoFilePath = await downloadAudio(link);
+	   const audioFilePath = await downloadAudio(link);
  
 	   // Upload the MP3 file to S3
-	   const key = `videos/${currentDate}/${path.basename(videoFilePath)}`;
-	   await uploadToS3(videoFilePath, key);
+	   const key = `audios/${currentDate}/${path.basename(audioFilePath)}`;
+	   await uploadToS3(audioFilePath, key);
  
 	   console.log("Downloaded, converted, and uploaded to S3 successfully! 🎉");
 	} catch (error) {
@@ -62,21 +57,21 @@ const downloadAudio = async (youtubeUrl: string): Promise<string> => {
 	   const outputFilePath = `${__dirname}/downloads/${videoInfo.videoDetails.title}.mp3`;
  
 	   const proc = ffmpeg({ source: audioStream })
-		  .withAudioCodec("libmp3lame")
-		  .toFormat("mp3")
-		  .on("end", () => {
-			 console.log("Video successfully converted to MP3");
-		  })
-		  .on("error", (error, stdout, stderr) => {
-			 console.error("Error converting video to MP3:", error);
-			 console.error("ffmpeg stdout:", stdout);
-			 console.error("ffmpeg stderr:", stderr);
-		  });
- 
+			.withAudioCodec("libmp3lame")
+			.toFormat("mp3")
+			.on("end", () => {
+				console.log("Video successfully converted to MP3");
+			})
+			.on("error", (error, stdout, stderr) => {
+				console.error("Error converting video to MP3:", error);
+				console.error("ffmpeg stdout:", stdout);
+				console.error("ffmpeg stderr:", stderr);
+			});
+	
 	   await new Promise<void>((resolve, reject) => {
-		  proc.saveToFile(outputFilePath)
-			 .on("end", resolve)
-			 .on("error", reject);
+			proc.saveToFile(outputFilePath)
+				.on("end", resolve)
+				.on("error", reject);
 	   });
  
 	   return outputFilePath;
@@ -88,22 +83,21 @@ const downloadAudio = async (youtubeUrl: string): Promise<string> => {
 };
  
  
-const uploadToS3 = async (videoFilePath: string, key: string) => {
+const uploadToS3 = async (audioFilePath: string, key: string) => {
 	const uploadParams = {
 	   Bucket: bucketName,
 	   Key: key,
-	   Body: await fs.readFile(videoFilePath), // Read the MP3 file from the local path
+	   Body: await fs.readFile(audioFilePath), // Read the MP3 file from the local path
 	};
  
 	try {
 	   const uploadResult = await s3Client.send(new PutObjectCommand(uploadParams));
 	   console.log("MP3 file uploaded to S3:", uploadResult);
+	   await fs.unlink(audioFilePath);
 	} catch (error) {
 	   console.error("Error uploading MP3 file to S3:", error);
 	   throw error;
 	}
 };
- 
-
 
 export default router;
